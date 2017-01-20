af547fd26f973f5bc135ee5d9f28fa0bd04b5593
miquelgimenez <miquel.nec31@gmail.com>
2017-01-20 18:18:22 +0100

    //First push

---
+++ app/src/main/java/gimenez/colomo/colomo_gimenez_practica_multimedia_android_m08uf2/MainActivity.java
@@ -0,0 +1,14 @@
+package gimenez.colomo.colomo_gimenez_practica_multimedia_android_m08uf2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //hey!
+    }
+}
